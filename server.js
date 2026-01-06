09eeb59c397175d9b46dab9aef394fc01b8b1824
Tatevik <tatevik2077@gmail.com>
2026-01-07 00:24:15 +0300

    Изменён файл server.js, добавлено подключение к redis

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pathc = require("path");
+const redis = require('redis');
 
+
+/*
+Неэффективный код, поменяю на app.use, чтобы программа была эфективней
 app.get('/',(req,res)=>{
-    res.send('web-client is running')
+    res.sendFile(path.join(project,'index.html'))
 });
+*/
+
+const redisClient = redis.createClient();
+(async () => {
+    await redisclient.connect();
+})();
+console.log("Connecting to the Redis");
+redisclient.on("ready", () => {
+    console.log("Connected!");
+});
+redisclient.on("error", (err) => {
+    console.log("Error in the Connection");
+});
+
 
 app.listen(port,()=>{
     console.log("web-client запущен")
-}
-
-)
+});
