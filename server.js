112177b98388e7b1fd914871299d19c24faa687f
Tatevik <tatevik2077@gmail.com>
2026-01-07 23:34:10 +0300

    Добавлен основной обработчик аутентификации в server.js и обновлен json файл

--- server.js
+++ server.js
@@ -3,15 +3,9 @@ const app = express();
 const port = 3000;
 const pathc = require("path");
 const redis = require('redis');
+const cookieParser = require('cooki-parser');
 
-
-/*
-Неэффективный код, поменяю на app.use, чтобы программа была эфективней
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(project,'index.html'))
-});
-*/
-
+//подключение к redis
 const redisClient = redis.createClient();
 (async () => {
     await redisclient.connect();
@@ -21,9 +15,43 @@ redisclient.on("ready", () => {
     console.log("Connected!");
 });
 redisclient.on("error", (err) => {
-    console.log("Error in the Connection");
+    console.log("Error in the Connection",err);
 });
 
+
+app.use(cookieParser(''));
+
+
+app.use(async(req,res,next)=>{
+    let sessionToken = req.cookies['session_id'];
+    if (!sessionToken){
+        console.log('неизвестный пользователь');          
+        return handleUnknowUser(req,res);
+    }
+
+
+    const userData = await 
+    redisClient.get(sessionToken);
+
+
+
+    if (!userData){
+        console.log('неизвестный пользователь');          
+        return handleUnknowUser(req,res);
+    }
+
+
+    const userData2 = JSON.parse(userData);
+    console.log('статус пользователя', userData);
+
+    if (userData === "Anonymous"){
+        return handleAnonymousUser(req,res,sessionToken,userData);
+    }else if (userData === "Authorized"){
+        return handleAuthorizedUser(req,res,sessionToken,userData);
+    }else{
+        return handleUnknowUser(req,res);
+    }
+});
 
 app.listen(port,()=>{
     console.log("web-client запущен")
